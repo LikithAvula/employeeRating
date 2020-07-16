a5e06e538fb5a4f54f32863f2f04634cdb1bea75
likith <likith.kumar91@gmail.com>
2020-07-16 18:36:07 -0400

    adding pid file

--- src/main/java/com/likith/EmployeesRating/EmployeesRatingApplication.java
+++ src/main/java/com/likith/EmployeesRating/EmployeesRatingApplication.java
@@ -2,6 +2,7 @@ package com.likith.EmployeesRating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.ApplicationContext;
 
 import com.likith.EmployeesRating.restController.EmployeeRatingController;
@@ -11,7 +12,11 @@ import com.likith.EmployeesRating.restController.EmployeeRatingController;
 public class EmployeesRatingApplication {
 	public static void main(String[] args) {
 		
-		SpringApplication.run(EmployeesRatingApplication.class, args);
+		//SpringApplication.run(EmployeesRatingApplication.class, args);
+		SpringApplication app = new SpringApplication(EmployeesRatingApplication.class);
+		app.addListeners(new ApplicationPidFileWriter("/pid/application.pid"));
+		app.run(args);
+		
 	}
 
 }
