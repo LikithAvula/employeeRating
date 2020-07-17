5abfb8c2f11c1f3b8c9d4278265009fd448b3c6c
likith <likith.kumar91@gmail.com>
2020-07-17 19:27:55 -0400

    changes to start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 
 cd ../../../../../../../../../
 cd /home/ec2-user/employee_rating_codeDeploy/target
-sudo /usr/bin/java -jar EmployeesRating-0.0.1-SNAPSHOT.jar
+sudo java -jar EmployeesRating-0.0.1-SNAPSHOT.jar
