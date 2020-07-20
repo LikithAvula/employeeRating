36aed38130d8581c44d7d7ec3aef07285ee89d16
likith <likith.kumar91@gmail.com>
2020-07-20 18:43:11 -0400

    adsfgasf

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@
 cd ../../../../../../../../../
 cd /home/ec2-user/employee_rating_codeDeploy/target
 nohup java -jar EmployeesRating-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+echo $! > /home/ec2-user/employee_rating_codeDeploy/pid/pid.file
